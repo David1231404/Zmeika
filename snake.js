49c100b24ac0e090e6155917060492a435fb4a70
David <davidkachaanov2004@gmail.com>
2024-09-17 20:30:34 +0300

    змейкка двигается и изгибается.

--- snake.js
+++ snake.js
@@ -3,40 +3,68 @@ var boardWidth = 1280;
 var boardHeight = 800;
 
 class Segm{
-    constructor(left, top){
-        this.left=Math.round(left);
-        this.top=Math.round(top);
-        //скорость змейки
-        this.speed=125;
-        this.showme = document.createElement('div');
+    constructor(left, top, itsclass){
+        this.__size = 20; //px
+        this.x = Math.round(left / this.__size);
+        this.y = Math.round(top / this.__size);
+        this.left = this.x * this.__size;
+        this.top = this.y * this.__size;
+        this.showme = document.createElement('input');
+        this.showme.setAttribute('type', 'button');
+        this.showme.setAttribute('class', itsclass);
+        this.showme.style.width = this.__size + 'px';
+        this.showme.style.height = this.__size + 'px';
     }
-    move(right, up) {  // на сколько сдвинуться вправо и вверх 5, 8; -100, -70
-        this.left += right;
-        this.top -= up;
-        this.showme.style.left=this.left + 'px';
-        this.showme.style.top=this.top + 'px';
+    setAttribute(n, v) {
+        this.showme.setAttribute(n, v);
+    }
+    move(right, up) {  // перемещение в ФИКСИРОВАННУЮ  точку в условных координатах!
+        this.x = right;
+        this.y = up;
+        this.left = this.__size * this.x;
+        this.top  = this.__size * this.y;
+        this.showme.style.left = this.left + 'px';
+        this.showme.style.top  = this.top + 'px';
     }
     render(){
-        console.log(this.left, this.top);
-        
-        this.showme.setAttribute('class', 'segm');
         this.showme.style.left=this.left + 'px';
         this.showme.style.top=this.top + 'px';
-        //console.log(this.showme.style);
         return this.showme;
     }
 }
 
 class Snake extends Array {
+    constructor() {
+        super(...arguments)  // распаковка аргументов по позициям
+        //скорость змейки
+        this.speed=125;
+        this.__dy = 0; // +1 move down each step -1 move up
+        this.__dx = 1; // +1 move right; -1 move left
+    }
     render(gameBoard) {
         for (let i = 0; i < this.length; i++) {
+            console.log(this[i].constructor);
             gameBoard.appendChild(this[i].render());
         }
     }
-    move(right, up) {
-        console.log('Snake moved');
-        for (let i = 0; i < this.length; i++) {
-            this[i].move(right, up);
+    move() {
+        if (this.length) {
+            console.log('Я ползу!');
+            for (let i = this.length - 1; i > 0; i--) {  // Перебираем змейку с хвоста
+                //console.log(i, this[i].x, this[i].y);
+                this[i].move(this[i-1].x, this[i-1].y);
+                //console.log(i, this[i].x, this[i].y);
+                //console.log();
+            }
+            console.log(0, this[0].x, this[0].y);
+            this[0].move(this[0].x + this.__dx, this[0].y + this.__dy);
+            console.log(0, this[0].x, this[0].y);
+            //for (let i = 0; i < this.length - 1; i++) {
+            //    this[i].move(this[i+1].x, this[i+1].y);
+            //}
+            //this[this.length - 1].move(this.x + this.__dx, this.y + this.__dy);
+        } else {
+            console.log('Меня нет!');
         }
     }
 }
@@ -46,33 +74,26 @@ class Snake extends Array {
 2. По таймеру, но "нормально"
 3. По таймеру, а по кнопке "изгибатся" и ползти в нужном направлении */
 
-class Apple{
-    constructor(left, top){
-        this.left=Math.round(left);
-        this.top=Math.round(top);
-    }
-    render(){
-        let result = document.createElement('div');
-        result.setAttribute('class', 'apple');
-        result.style.left=this.left + 'px';
-        result.style.top=this.top + 'px';
-        return result;
-    }
+class Apple extends Segm{
 }
 
 function addobjects() {
     let snake = new Snake;
     
-    for (let i = 0; i < 4; i++) { 
-        snake.push(new Segm(
+    for (let i = 0; i < 10; i++) { 
+        let ss = new Segm(
             (boardWidth - 20) / 2, 
-            (boardHeight - 20) / 2 - (i * 20) 
-        ));
+            (boardHeight - 20) / 2 - (i * 20),
+            'segm'
+        );
+        ss.setAttribute('value', i);
+        snake.push(ss);
     }
 
     let apple = new Apple(
         Math.random() * (boardWidth - 20), 
-        Math.random() * (boardHeight - 20)
+        Math.random() * (boardHeight - 20),
+        'apple'
     );
 
     snake.render(gameBoard);
